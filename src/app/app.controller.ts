9b5da2c033b7425922997f2d9bdcf21287ad6949
Matpdev <matheus2ep@gmail.com>
2023-05-17 21:12:16 -0300

    FIX: Fixed healthy

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -6,12 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/oi')
-  getOi() {
-    return this.appService.sendHello();
+  getStatusOk(): string {
+    return this.appService.getOk();
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,11 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getOk(): string {
     return 'OK!';
-  }
-
-  sendHello() {
-    return { Oi: 'Oi' };
   }
 }
