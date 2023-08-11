ab6b31fce93e3cf3efe1b39c9dc702323ac4b1b5
Matpdev <matheus2ep@gmail.com>
2023-08-11 14:16:09 -0300

    FIX(api): Fixed tests

--- src/send-media/send-media.controller.spec.ts
+++ src/send-media/send-media.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SendImagesController } from './send-media.controller';
 import { SendImagesService } from './send-media.service';
 import { UploaderService } from 'src/singleServices/uploader.service';
+import { PrismaService } from 'src/singleServices/prisma.service';
 
 describe('SendImagesController', () => {
   let controller: SendImagesController;
@@ -9,7 +10,7 @@ describe('SendImagesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SendImagesController],
-      providers: [SendImagesService, UploaderService],
+      providers: [SendImagesService, UploaderService, PrismaService],
     }).compile();
 
     controller = module.get<SendImagesController>(SendImagesController);
--- src/send-media/send-media.service.spec.ts
+++ src/send-media/send-media.service.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SendImagesService } from './send-media.service';
 import { UploaderService } from 'src/singleServices/uploader.service';
+import { PrismaService } from 'src/singleServices/prisma.service';
 
 describe('SendImagesService', () => {
   let service: SendImagesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SendImagesService, UploaderService],
+      providers: [SendImagesService, UploaderService, PrismaService],
     }).compile();
 
     service = module.get<SendImagesService>(SendImagesService);
